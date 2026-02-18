1ec941c7c85058061d1faf96352329614877b8d9
Kathryn Audrey <12405269+kaudreydev@users.noreply.github.com>
2026-02-18 11:32:34 -0500

    Formatted Games data using Table component

--- src/app/games/game-list.tsx
+++ src/app/games/game-list.tsx
@@ -1,5 +1,14 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
 import { Game } from "@/lib/models/game";
-import { JSX, use, Suspense } from "react";
+import { JSX, Suspense, use } from "react";
 
 export default function GameList({
     games,
@@ -15,13 +24,23 @@ export default function GameList({
             <Suspense fallback={<div>Loading...</div>}>
                 {showGames && (
                     <div>
-                        <ul>
-                            {allGames.map((game: Game) => (
-                                <li key={game.gameId.toString()}>
-                                    {game.igdbId}
-                                </li>
-                            ))}
-                        </ul>
+                        <Table>
+                            <TableCaption>List of Games</TableCaption>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">
+                                        IGDB ID
+                                    </TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {allGames.map((game: Game) => (
+                                    <TableRow key={game.gameId.toString()}>
+                                        <TableCell>{game.igdbId}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
                     </div>
                 )}
                 {!showGames && <div>Failed to retrieve games.</div>}
